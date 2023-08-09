125957673a3d5fe32b183a2504ee5bf3aa3a77cb
Lebogang <lebogangodeveloper@gmail.com>
2023-08-09 11:48:59 +0200

    Define the BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""base_model module"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    BaseModel class that defines common attributes/methods for
+    other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the BaseModel instance."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Convert the BaseModel instance to a dictionary."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
