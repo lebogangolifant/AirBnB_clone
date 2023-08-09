86777bcf8dd7f3a3b7e4dab0a3714bb435a97601
Lebogang <lebogangodeveloper@gmail.com>
2023-08-09 16:02:18 +0200

    Initialize the BaseModel instance

--- models/base_model.py
+++ models/base_model.py
@@ -25,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""
@@ -34,6 +35,7 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Convert the BaseModel instance to a dictionary."""
